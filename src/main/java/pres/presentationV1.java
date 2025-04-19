b6c9c4d1cea04a1c996ee1c5777df0a17334d135
ASMAE <asmaezineel@gmail.com>
2025-04-19 20:16:29 +0100

    init commit

--- src/main/java/pres/presentationV1.java
+++ src/main/java/pres/presentationV1.java
@@ -1,10 +1,18 @@
 package pres;
 
+import dao.DaoImpl;
+import ext.DaoImpV2;
 import metier.MetierImpl;
 
 public class presentationV1 {
     public static void main(String[] args) {
-        MetierImpl metier = new MetierImpl();
-        System.out.println(metier.calcul());
+        /*
+        injection des dependances par instanciation statique=>NEW
+         */
+
+        DaoImpl d = new DaoImpl();
+        MetierImpl metier = new MetierImpl(d); // injection des dependances via le constructeur
+        //metier.setDao(d);//Injection via le setter
+        System.out.println("RES="+metier.calcul());
     }
 }
